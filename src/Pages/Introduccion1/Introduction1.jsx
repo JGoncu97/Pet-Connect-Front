cfc799ec30e04d0b249bcba7d43724b9cb741905
JGoncu97 <jggc1197@gmail.com>
2025-04-23 16:54:03 -0500

    Prueba 4 Introduccion 1

--- src/Pages/Introduccion1/Introduction1.jsx
+++ src/Pages/Introduccion1/Introduction1.jsx
@@ -9,11 +9,11 @@ export const Introduction1 = () => {
     }
 
     return (
-        <div className='h-auto flex flex-col  '>
-            <div className="flex-1  relative overflow-hidden border pt-0">
+        <div className='min-h-screen flex flex-col  '>
+            <div className="flex-1  relative overflow-hidden ">
                 {/* Sección superior con imagen */}
                 <div 
-                    className="w-full p-4 bg-orange-400   h-[60vh] xs:h-[95vh] sm:h-[90vh] md:h-[90vh] lg:h-[90vh]"
+                    className="w-full p-7 bg-orange-400  h-[65vh] xs:h-[80vh] sm:h-[90vh] md:h-[90vh] lg:h-[90vh]"
                     style={{ 
                         clipPath: 'ellipse(100% 420px at center top)'
                     }}
@@ -21,13 +21,13 @@ export const Introduction1 = () => {
                     <img 
                         src={Intro} 
                         alt="Introducción" 
-                        className="w-full max-w-md h-auto object-contain"
+                        className="w-full max-w-md  object-contain"
                     />
                 </div>
 
                 {/* Sección inferior con contenido */}
-                <div className="absolute bottom-0 left-0 right-0 z-20 bottom-8 border">
-                    <div className="p-6 text-center bg-white  shadow-lg border">
+                <div className="absolute bottom-0 left-0 right-0 z-20 bottom-18 h-auto ">
+                    <div className="p-6 text-center bg-white  shadow-lg ">
                         <h2 className="text-xl font-bold mb-4">Más que una app, un puente hacia el hogar</h2>
                         <p className="text-gray-600 mb-8">
                             Cuando una mascota se pierde, cada segundo cuenta. Un escaneo y el reencuentro comienza.
