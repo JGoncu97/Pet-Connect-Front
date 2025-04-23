418f78a0e4eeb3cb96be7d13a5dc65bfe7a5cec8
JGoncu97 <jggc1197@gmail.com>
2025-04-23 10:53:48 -0500

    Fix Responsive XS

--- src/Pages/Introduccion1/Introduction1.jsx
+++ src/Pages/Introduccion1/Introduction1.jsx
@@ -12,12 +12,12 @@ export const Introduction1 = () => {
 
 
     return (
-        <div className="flex flex-col items-center justify-center  bg-gray-100 ">
-            <div className="min-h-screen relative items-center justify-end bg-white p-2 relative overflow-hidden ">
+        <div>
+            <div className="h-screen p-2 relative overflow-hidden ">
                 
                 <div 
                     className="absolute inset-0 bg-orange-400 flex justify-center items-end h-[95vh] xs:h-[95vh] sm:h-[35vh] md:h-[30vh] lg:h-[88vh] rounded-b-[5rem]"
-                    style={{ clipPath: 'ellipse(100% 550px at center top)' }}
+                    style={{ clipPath: 'ellipse(100% 500px at center top)' }}
                 >
                 
                     <img 
@@ -28,7 +28,7 @@ export const Introduction1 = () => {
                 </div>
 
             
-                <div className=" absolute top-150 xs:top-135 lg:top-90 relative w-full lg:w-screen  bg-white  flex flex-col items-center z-20  overflow-hidden   ">
+                <div className=" absolute top-125 xs:top-145 lg:top-90 relative w-full lg:w-screen  bg-white  flex flex-col items-center z-20  overflow-hidden   ">
                 
                     <div className="p-6 text-center w-full  flex flex-col items-center rounded-lg mb-10">
                         <h2 className="text-xl font-bold">Más que una app, un puente hacia el hogar</h2>
@@ -37,7 +37,7 @@ export const Introduction1 = () => {
                         </p>
 
                     
-                        <button  onClick={handleContinue}  className="mt-16 xs:mt-8 w-full max-w-[20rem] py-3 bg-orange-400 text-white font-semibold rounded-full shadow-md">
+                        <button  onClick={handleContinue}  className="mt-8 xs:mt-8 w-full max-w-[20rem] py-3 bg-orange-400 text-white font-semibold rounded-full shadow-md">
                             Continuar
                         </button>
                     </div>
